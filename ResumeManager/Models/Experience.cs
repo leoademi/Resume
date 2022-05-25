1a9c95d01b8c845ede5233eb951c72c244d40eee
LeoAdemi <Admin@LEO>
2022-05-25 20:21:21 +0200

    l.a

---
+++ ResumeManager/Models/Experience.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResumeManager.Models
+{
+    public class Experience
+    {
+        public Experience()
+        {
+        }
+
+        [Key]
+        public int ExperienceId { get; set; }
+
+        [ForeignKey("Applicant")]//very important
+        public int ApplicantId { get; set; }
+        public virtual Applicant Applicant { get; private set; } //very important 
+
+        public string CompanyName { get; set; }
+        public string Designation { get; set; }
+        [Range(1,25,ErrorMessage ="Years must be between 1 and 25")]
+        [Required]
+        public int YearsWorked { get; set; }
+
+    }
+}
